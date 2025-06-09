30c464c5c8e29e88c6819d1a647c4ff9461d9d47
tr-Kalyan <kalyansde1@gmail.com>
2025-06-09 09:51:57 +0530

    Fix Netlify error with _redirects and plugin

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/_redirects',
+          dest: '.' // copies to dist/_redirects
+        }
+      ]
+    })
+  ]
+});
